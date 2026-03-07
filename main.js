fe841aa3dbafc7af27eeeb2f9f0005973f587f28
Roman <sfbackupservice@gmail.com>
2026-03-07 20:56:12 +0000

    Update website

---
+++ main.js
@@ -0,0 +1,17 @@
+function copyProdURL() {
+    const text = document.getElementById("prod-url").textContent;
+    navigator.clipboard.writeText(text)
+        .then(() => alert("Production URL copied!"));
+}
+
+function copySandboxURL() {
+    const text = document.getElementById("sandbox-url").textContent;
+    navigator.clipboard.writeText(text)
+        .then(() => alert("Sandbox URL copied!"));
+}
+
+function copyEmail() {
+    const text = document.getElementById("email-text").textContent;
+    navigator.clipboard.writeText(text)
+        .then(() => alert("Email copied!"));
+}
